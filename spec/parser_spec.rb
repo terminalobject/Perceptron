c05a14e671a1e5b561868f166b30a34d175b5f90
jinimcoroneo <jinim1983@gmail.com>
2017-08-18 17:26:55 +0100

    refactor perceptron and parser

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -7,8 +7,8 @@ describe Parser do
     context 'Inputs considered bad' do
       subject(:parser) { described_class.new }
 
-      let(:first_output)  { { vector: [1, 8, 1, 2], expected: 1 } }
-      let(:second_output) { { vector: [1, 12, 1, 3], expected: 1 } }
+      let(:first_output)  { { vector: [1, 1, 2, 0], expected: 1 } }
+      let(:second_output) { { vector: [1, 1, 3, 0], expected: 1 } }
       let(:output)        { [first_output, second_output] }
 
       it 'returns a hash of input vectors' do
@@ -21,8 +21,8 @@ describe Parser do
     context 'Inputs considered good' do
       subject(:parser) { described_class.new }
 
-      let(:first_output)  { { vector: [1, 12, 0, 0], expected: 0 } }
-      let(:second_output) { { vector: [1, 8, 0, 0], expected: 0 } }
+      let(:first_output)  { { vector: [1, 0, 0, 0], expected: 0 } }
+      let(:second_output) { { vector: [1, 0, 0, 0], expected: 0 } }
       let(:output)        { [first_output, second_output] }
 
       it 'returns a hash of input vectors' do
